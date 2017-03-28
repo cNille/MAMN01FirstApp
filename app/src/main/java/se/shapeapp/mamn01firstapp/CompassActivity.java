c7edddb32874b6bcb5561f18dd077df94f01955d
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-03-28 17:51:39 +0200

    Add sound + vibration to compass

--- app/src/main/java/se/shapeapp/mamn01firstapp/CompassActivity.java
+++ app/src/main/java/se/shapeapp/mamn01firstapp/CompassActivity.java
@@ -1,11 +1,18 @@
 package se.shapeapp.mamn01firstapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -26,6 +33,9 @@ public class CompassActivity extends Activity implements SensorEventListener {
     private float[] mOrientation = new float[3];
     private float mCurrentDegree = 0f;
     private TextView tvHeading;
+    private Vibrator v;
+    private long lastUpdate;
+    private long currentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,10 @@ public class CompassActivity extends Activity implements SensorEventListener {
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mPointer = (ImageView) findViewById(R.id.pointer);
+        tvHeading = (TextView) findViewById(R.id.compasscoord);
+        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
+
     }
 
     protected void onResume() {
@@ -53,12 +67,19 @@ public class CompassActivity extends Activity implements SensorEventListener {
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             mLastAccelerometer = lowPass(event.values.clone(), mLastAccelerometer);
+            mLastAccelerometerSet = true;
         } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             mLastMagnetometer = lowPass(event.values.clone(), mLastMagnetometer);
+            mLastMagnetometerSet = true;
         }
-        if (mLastAccelerometer != null && mLastMagnetometer != null) {
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
             SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
             SensorManager.getOrientation(mR, mOrientation);
+            float x = (float) Math.floor(mOrientation[0] * 1000) / 1000;
+            float y = (float) Math.floor(mOrientation[1] * 1000) / 1000;
+            float z = (float) Math.floor(mOrientation[2] * 1000) / 1000;
+            tvHeading.setText("Orientation\nX: " + String.valueOf(x) + "\nY: " + String.valueOf(y) + "\nZ: " + z );
+
             float azimuthInRadians = mOrientation[0];
             float azimuthInDegrees = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
             RotateAnimation ra = new RotateAnimation(
@@ -69,9 +90,72 @@ public class CompassActivity extends Activity implements SensorEventListener {
                     0.5f);
             mPointer.startAnimation(ra);
             mCurrentDegree = -azimuthInDegrees;
+
+            currentTime = System.currentTimeMillis();
+            if(Math.abs(lastUpdate - currentTime) > 1000){
+                lastUpdate = currentTime;
+                // Vibrate when orientation close to North pole.
+                if(Math.abs(mOrientation[0]) < 0.1){
+                    v.vibrate(200); // Vibrate for 200 milliseconds
+                }
+                // Make a higher pitch sound when close to north pole
+                // Use a new tread as this can take a while
+                final Thread thread = new Thread(new Runnable() {
+                    public void run() {
+                        genTone(1000 - (200 * Math.abs(mOrientation[0])) );
+                        handler.post(new Runnable() {
+
+                            public void run() {
+                                playSound();
+                            }
+                        });
+                    }
+                });
+                thread.start();
+            }
         }
     }
 
+    private final float duration = 0.3f; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = Math.round(duration * sampleRate);
+    private final double sample[] = new double[numSamples];
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
+    void genTone(float tone){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            //sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/tone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+        }
+    }
+
+    void playSound(){
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, numSamples,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
+        try {
+            Thread.sleep(1000);
+            audioTrack.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     static final float ALPHA = 0.25f; // if ALPHA = 1 OR 0, no filter applies.
     protected float[] lowPass( float[] input, float[] output ) {
         if ( output == null ) return input;
@@ -85,5 +169,4 @@ public class CompassActivity extends Activity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Do nothing
     }
-
 }
